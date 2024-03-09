c7989ef424f669ef2fd1c998e993dbbe8e17347a
elderberry17 <bezgin14a@mail.ru>
2024-03-09 17:36:17 +0300

    initial commit

---
+++ connection.py
@@ -0,0 +1,19 @@
+import os
+
+from dotenv import load_dotenv
+from sqlmodel import SQLModel, Session, create_engine
+
+load_dotenv()
+# db_url = os.getenv('DB_ADMIN')
+db_url = "postgresql://agbezgin:agbezgin@localhost/hack"
+engine = create_engine(db_url, echo=True)
+
+print(engine)
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
